e619fdf7aabd8463ee8f209c95928aa23f46040a
MariaGuilarte <mariajoseguilarte@gmail.com>
2019-04-13 13:00:52 -0430

    DevelopmetStatus migration fields, done.

--- database/migrations/2019_04_13_172629_create_development_statuses_table.php
+++ database/migrations/2019_04_13_172629_create_development_statuses_table.php
@@ -4,28 +4,17 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDevelopmentStatusesTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('development_statuses', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
-        });
-    }
+class CreateDevelopmentStatusesTable extends Migration {
+  
+  public function up() {
+    Schema::create('development_statuses', function (Blueprint $table) {
+      $table->bigIncrements('id');
+      $table->string('name', 191);
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('development_statuses');
-    }
+  public function down() {
+    Schema::dropIfExists('development_statuses');
+  }
 }
