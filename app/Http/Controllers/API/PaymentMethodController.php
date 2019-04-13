a0be7c93ad0a8709042e740798b7b752cc715fd6
MariaGuilarte <mariajoseguilarte@gmail.com>
2019-04-13 15:50:34 -0430

    PaymentMethod controller sketched. Empty initialization.

--- app/Http/Controllers/API/PaymentMethodController.php
+++ app/Http/Controllers/API/PaymentMethodController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
 use App\PaymentMethod;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class PaymentMethodController extends Controller
 {
