f0cf09cad509a1103c63139d3700edf98d46e70c
MariaGuilarte <mariajoseguilarte@gmail.com>
2019-04-13 13:02:23 -0430

    DevelopmentStatus resource initial settings, done.

--- app/Http/Resources/DevelopmentStatusResource.php
+++ app/Http/Resources/DevelopmentStatusResource.php
@@ -4,16 +4,9 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class DevelopmentStatusResource extends JsonResource
-{
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    public function toArray($request)
-    {
-        return parent::toArray($request);
-    }
+class DevelopmentStatusResource extends JsonResource {
+  
+  public function toArray($request) {
+    return parent::toArray($request);
+  }
 }
