b0279e0232965c0099c2b6d5dfa30a7f07996757
MariaGuilarte <mariajoseguilarte@gmail.com>
2019-04-13 12:13:47 -0430

    UpdateClient request created, initial settings done.

--- app/Http/Requests/UpdateClient.php
+++ app/Http/Requests/UpdateClient.php
@@ -4,27 +4,19 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateClient extends FormRequest
-{
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return false;
-    }
+class UpdateClient extends FormRequest {
+  
+  public function authorize() {
+    return true;
+  }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return [
-            //
-        ];
-    }
+  public function rules()
+  {
+    return [
+      'name'         => 'required|string|max:191',
+      'email'        => 'required|string|max:191',
+      'phone_number' => 'nullable|string|max:191',
+      'country'      => 'required|string|max:191'
+    ];
+  }
 }
