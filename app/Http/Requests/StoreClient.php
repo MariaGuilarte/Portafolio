455d6c0a254d431ffc49d6bcbe365e2b914ae000
MariaGuilarte <mariajoseguilarte@gmail.com>
2019-04-13 12:13:08 -0430

    StoreClient request created, initial settings done.

--- app/Http/Requests/StoreClient.php
+++ app/Http/Requests/StoreClient.php
@@ -4,27 +4,18 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreClient extends FormRequest
-{
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return false;
-    }
+class StoreClient extends FormRequest {
+  
+  public function authorize() {
+    return true;
+  }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return [
-            //
-        ];
-    }
+  public function rules() {
+    return [
+      'name'         => 'required|string|max:191',
+      'email'        => 'required|string|max:191',
+      'phone_number' => 'nullable|string|max:191',
+      'country'      => 'required|string|max:191'
+    ];
+  }
 }
