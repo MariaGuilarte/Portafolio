578ab3b9aae75bcb5b100d0de7d588fd80b2da2c
MariaGuilarte <mariajoseguilarte@gmail.com>
2019-04-13 12:25:50 -0430

    Client API Routes added

--- routes/api.php
+++ routes/api.php
@@ -14,5 +14,12 @@ use Illuminate\Http\Request;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
 });
+
+Route::post('clients/{client}', 'API\ClientController@update');
+
+Route::resources(
+  [
+    'clients' => 'API\ClientController'
+  ]);
