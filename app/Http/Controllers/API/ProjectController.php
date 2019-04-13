11db625449602fba3fdee9420c3108ad1886bae2
MariaGuilarte <mariajoseguilarte@gmail.com>
2019-04-13 16:30:46 -0430

    Project controller, initial settings done.

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -5,82 +5,65 @@ namespace App\Http\Controllers\API;
 use App\Project;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ProjectResource;
+use App\Http\Requests\StoreProject;
+use App\Http\Requests\UpdateProject;
 
-class ProjectController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+class ProjectController extends Controller {
+  
+  public function index() {
+    $projects = Project::all();
+    return ProjectResource::collection( $projects );
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  public function create() {
+    return view('projects.create');
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  public function store(StoreProject $request) {
+    
+    $project = Project::create([
+      'name'                  => $request->name,
+      'description'           => $request->description,
+      'start_date'            => $request->start_date,
+      'end_date'              => $request->end_date,
+      'budget'                => $request->budget,
+      'client_id'             => $request->client_id,
+      'platform_id'           => $request->platform_id,
+      'development_status_id' => $request->development_status_id,
+      'payment_method_id'     => $request->payment_method_id
+    ]);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Project  $project
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Project $project)
-    {
-        //
-    }
+    return new ProjectResource( $project );
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Project  $project
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Project $project)
-    {
-        //
-    }
+  public function show(Project $project) {
+    return new ProjectResource( $project );
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Project  $project
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Project $project)
-    {
-        //
-    }
+  public function edit(Project $project) {
+    return view('projects.edit', ['project' => $project]);
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Project  $project
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Project $project)
-    {
-        //
-    }
+  public function update(UpdateProject $request, Project $project) {
+    
+    $project->update([
+      'name'                  => $request->name,
+      'description'           => $request->description,
+      'start_date'            => $request->start_date,
+      'end_date'              => $request->end_date,
+      'budget'                => $request->budget,
+      'client_id'             => $request->client_id,
+      'platform_id'           => $request->platform_id,
+      'development_status_id' => $request->development_status_id,
+      'payment_method_id'     => $request->payment_method_id
+    ]);
+
+    return new ProjectResource( $project );
+  }
+
+  public function destroy(Project $project) {
+    $project->delete();
+  }
+  
 }
