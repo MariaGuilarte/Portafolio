44b0a3bb93bdab4d3706f278dc2667e3e5d3223e
MariaGuilarte <mariajoseguilarte@gmail.com>
2019-04-13 13:04:17 -0430

    UpdateDevelopmentStatus request initial settings done.

--- app/Http/Requests/UpdateDevelopmentStatus.php
+++ app/Http/Requests/UpdateDevelopmentStatus.php
@@ -4,27 +4,15 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateDevelopmentStatus extends FormRequest
-{
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return false;
-    }
+class UpdateDevelopmentStatus extends FormRequest {
+  
+  public function authorize() {
+    return true;
+  }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return [
-            //
-        ];
-    }
+  public function rules() {
+    return [
+      'name' => 'required|string|max:191'
+    ];
+  }
 }
