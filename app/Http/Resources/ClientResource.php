8dee12429c4ac81c8da9dca5c06ed90c84950081
MariaGuilarte <mariajoseguilarte@gmail.com>
2019-04-13 12:08:55 -0430

    Client resource initial settings, done.

--- app/Http/Resources/ClientResource.php
+++ app/Http/Resources/ClientResource.php
@@ -4,16 +4,10 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class ClientResource extends JsonResource
-{
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    public function toArray($request)
-    {
-        return parent::toArray($request);
-    }
+class ClientResource extends JsonResource {
+  
+  public function toArray($request) {
+    return parent::toArray($request);
+  }
+  
 }
