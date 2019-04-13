a014406ac15d3940a346906e4d7e66799d3c6bec
MariaGuilarte <mariajoseguilarte@gmail.com>
2019-04-13 16:11:13 -0430

    Project Resource initial settings, done.

--- app/Http/Resources/ProjectResource.php
+++ app/Http/Resources/ProjectResource.php
@@ -4,16 +4,10 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class ProjectResource extends JsonResource
-{
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    public function toArray($request)
-    {
-        return parent::toArray($request);
-    }
+class ProjectResource extends JsonResource {
+  
+  public function toArray($request) {
+    return parent::toArray($request);
+  }
+  
 }
