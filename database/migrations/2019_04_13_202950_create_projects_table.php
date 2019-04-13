7a9dc24388b252db08ec420055e13a4445f24bad
MariaGuilarte <mariajoseguilarte@gmail.com>
2019-04-13 16:07:13 -0430

    Project migration fields, done.

--- database/migrations/2019_04_13_202950_create_projects_table.php
+++ database/migrations/2019_04_13_202950_create_projects_table.php
@@ -4,28 +4,26 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProjectsTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('projects', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
-        });
-    }
+class CreateProjectsTable extends Migration {
+  
+  public function up() {
+    Schema::create('projects', function (Blueprint $table) {
+      $table->bigIncrements('id');
+      $table->string('name', 191);
+      $table->text('description');
+      $table->date('start_date');
+      $table->date('end_date');
+      $table->integer('budget');
+      
+      $table->bigUnsignedInteger('client_id');
+      $table->bigUnsignedInteger('platform_id');
+      $table->bigUnsignedInteger('development_status_id');
+      $table->bigUnsignedInteger('payment_method_id');
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('projects');
-    }
+  public function down() {
+    Schema::dropIfExists('projects');
+  }
 }
