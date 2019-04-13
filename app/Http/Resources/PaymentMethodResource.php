b5a727d9e17c83cd6c3b67edfef3c28f51b8d32d
MariaGuilarte <mariajoseguilarte@gmail.com>
2019-04-13 15:43:20 -0430

    Platform resource initial settings, done.

--- app/Http/Resources/PaymentMethodResource.php
+++ app/Http/Resources/PaymentMethodResource.php
@@ -4,16 +4,10 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class PaymentMethodResource extends JsonResource
-{
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    public function toArray($request)
-    {
-        return parent::toArray($request);
-    }
+class PaymentMethodResource extends JsonResource {
+  
+  public function toArray($request) {
+    return parent::toArray($request);
+  }
+  
 }
