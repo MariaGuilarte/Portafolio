65c7214447f9110c03d8f75c348affa8051528c9
MariaGuilarte <mariajoseguilarte@gmail.com>
2019-04-13 15:48:01 -0430

    UpdatePaymentMethod form-request initial settings done.

--- app/Http/Requests/UpdatePaymentMethod.php
+++ app/Http/Requests/UpdatePaymentMethod.php
@@ -4,27 +4,18 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdatePaymentMethod extends FormRequest
-{
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return false;
-    }
+class UpdatePaymentMethod extends FormRequest {
+  
+  public function authorize() {
+    return true;
+  }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return [
-            //
-        ];
-    }
+  public function rules() {
+    return [
+      'name' => 'required|unique|string|max:191',
+      'logo' => 'nullable|string|max:191',
+      'url'  => 'required|unique|string|max:191'
+    ];
+  }
+  
 }
