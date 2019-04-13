cdd90f2a8186accaada656bb172238629666ff57
MariaGuilarte <mariajoseguilarte@gmail.com>
2019-04-13 12:22:33 -0430

    Client controller, initial settings done.

--- app/Http/Controllers/API/ClientController.php
+++ app/Http/Controllers/API/ClientController.php
@@ -5,82 +5,54 @@ namespace App\Http\Controllers\API;
 use App\Client;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ClientResource;
+use App\Http\Requests\StoreClient;
+use App\Http\Requests\UpdateClient;
 
-class ClientController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+class ClientController extends Controller {
+  
+  public function index() {
+    $clients = Client::all();
+    return ClientResource::collection( $clients );
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  public function create() {
+    return view('clients.create');
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  public function store(StoreClient $request) {
+    
+    $client = Client::create([
+      'name'         => $request->name,
+      'email'        => $request->email,
+      'phone_number' => $request->phone_number,
+      'country'      => $request->country
+    ]);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Client  $client
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Client $client)
-    {
-        //
-    }
+    return new ClientResource( $client );
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Client  $client
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Client $client)
-    {
-        //
-    }
+  public function show(Client $client) {
+    return new ClientResource( $client );
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Client  $client
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Client $client)
-    {
-        //
-    }
+  public function edit(Client $client) {
+    return view('clients.edit', ['client' => $client]);
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Client  $client
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Client $client)
-    {
-        //
-    }
+  public function update(UpdateClient $request, Client $client) {
+    
+    $client->update([
+      'name'         => $request->name,
+      'email'        => $request->email,
+      'phone_number' => $request->phone_number,
+      'country'      => $request->country
+    ]);
+
+    return new ClientResource( $client );
+  }
+
+  public function destroy(Client $client) {
+    $client->delete();
+  }
 }
